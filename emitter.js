0f77bca0e3d48d0ae109e0dc984ef411409e85bf
Anna Gromova <gromovaanka@gmail.com>
2018-11-06 18:06:31 +0500

    first version

--- emitter.js
+++ emitter.js
@@ -4,13 +4,44 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    let events = {};
+
+    function addNotification(event, subevent, context, handler) {
+        if (!events[event + subevent]) {
+            events[event + subevent] = [];
+        }
+        events[event + subevent].push({ context, handler });
+    }
+
+    function deleteNotification(event, subevent, context) {
+        Object.keys(events).filter(key =>
+            key === event + subevent || subevent === '' && key.split('.')[0] === event)
+            .forEach(key => {
+                events[key] = events[key]
+                    .filter(subscriber => subscriber.context !== context);
+            });
+    }
+
+    function emitNotification(event, subevent) {
+        if (events[event + subevent] && subevent !== '') {
+            events[event + subevent].forEach(subscriber => {
+                subscriber.handler.call(subscriber.context);
+            });
+        }
+        if (events[event]) {
+            events[event].forEach(subscriber => {
+                subscriber.handler.call(subscriber.context);
+            });
+        }
+    }
+
     return {
 
         /**
@@ -18,26 +49,53 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Any}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            let eventAndSubevent = event.split('.');
+            if (eventAndSubevent[1]) {
+                eventAndSubevent[1] = '.' + eventAndSubevent;
+            } else {
+                eventAndSubevent[1] = '';
+            }
+            addNotification(eventAndSubevent[0], eventAndSubevent[1], context, handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Any}
          */
         off: function (event, context) {
-            console.info(event, context);
+            let eventAndSubevent = event.split('.');
+            if (eventAndSubevent[1]) {
+                eventAndSubevent[1] = '.' + eventAndSubevent;
+            } else {
+                eventAndSubevent[1] = '';
+            }
+            deleteNotification(eventAndSubevent[0], eventAndSubevent[1], context);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Any}
          */
         emit: function (event) {
-            console.info(event);
+            let eventAndSubevent = event.split('.');
+            if (eventAndSubevent[1]) {
+                eventAndSubevent[1] = '.' + eventAndSubevent;
+            } else {
+                eventAndSubevent[1] = '';
+            }
+            emitNotification(eventAndSubevent[0], eventAndSubevent[1]);
+
+            return this;
         },
 
         /**
